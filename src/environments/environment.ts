c3b1997ab1ee76ae2a8ba5e6969907544bd65f56
herbataszatana <71353315+herbataszatana@users.noreply.github.com>
2023-02-13 22:25:04 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 
+# Update below with your project details 
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyBT_r_pkbaENCCahahE-4KPhgua5qiazKc",
-  authDomain: "notetaker-be894.firebaseapp.com",
-  projectId: "notetaker-be894",
-  storageBucket: "notetaker-be894.appspot.com",
-  messagingSenderId: "170442045466",
-  appId: "1:170442045466:web:6f1ed31beb57bb964b39a3"
+    apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
   },
   production: false
 };
